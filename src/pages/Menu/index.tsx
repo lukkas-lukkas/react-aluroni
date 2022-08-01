1e85f7342f6699394ac1a67ef9e7abb1214b5855
Lucas Lima <lukkasoliveiralima@gmail.com>
2022-08-01 18:04:12 -0300

    create container and header

--- src/pages/Menu/index.tsx
+++ src/pages/Menu/index.tsx
@@ -4,9 +4,14 @@ import style from "./Menu.module.scss";
 export default function Menu() {
     return (
         <main>
-            <nav className={style.menu}>
+            <nav className={`${style.menu} container`}>
                 <Logo />
             </nav>
+            <header className={style.header}>
+                <div className={`${style.header__text} container`}>
+                    Great code and spaghetti
+                </div>
+            </header>
         </main>
     );
 }
