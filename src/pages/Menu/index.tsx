9b3def52789c1ea6c44d63c64335f58dafe0bc26
Lucas Lima <lukkasoliveiralima@gmail.com>
2022-08-01 17:43:28 -0300

    add import of logo

---
+++ src/pages/Menu/index.tsx
@@ -0,0 +1,11 @@
+import { ReactComponent as Logo } from "../../assets/logo.svg";
+
+export default function Menu() {
+    return (
+        <main>
+            <nav className="menu">
+                <Logo />
+            </nav>
+        </main>
+    );
+}
