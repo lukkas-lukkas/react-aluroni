c61689544a79cfc4104c36c097cbf5d9c993aa53
Lucas Lima <lukkasoliveiralima@gmail.com>
2022-08-02 14:50:11 -0300

    add images to menu

--- src/pages/Menu/Itens/Item/index.tsx
+++ src/pages/Menu/Itens/Item/index.tsx
@@ -5,19 +5,19 @@ import itens from "../itens.json";
 type Props = typeof itens[0];
 
 export default function Item(props: Props) {
-    const { title, description, category, size, serving, price } = props;
+    const { title, description, category, size, serving, price, photo } = props;
 
     return (
         <div className={style.item}>
             <div className={style.item__image}>
-                <img src={logo} alt={title} />
+                <img src={photo} alt={title} />
             </div>
             <div className={style.item__description}>
                 <div className={style.item__title}>
                     <h2> {title} </h2>
                     <p> {description} </p>
                 </div>
-                <div className={style.item__tags}>
+                <div className={`${style.item__tags} ${style[`item__type__${category.label.toLocaleLowerCase()}`]}`}>
                     <div className={style.item__type}>{category.label}</div>
                     <div className={style.item__portion}>{size}g</div>
                     <div className={style.item__qttpeoples}>{serving} pessoa{serving === 1 ? "" : "s"}</div>
