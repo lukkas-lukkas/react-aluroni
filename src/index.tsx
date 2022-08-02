77eae35103aabe432033c21136a3529986f8cca9
Lucas Lima <lukkasoliveiralima@gmail.com>
2022-08-02 18:51:54 -0300

    fix eslint errors

--- src/index.tsx
+++ src/index.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import Menu from './pages/Menu';
-import "normalize.css";
+import 'normalize.css';
 
 const root = ReactDOM.createRoot(
   document.getElementById('root') as HTMLElement
