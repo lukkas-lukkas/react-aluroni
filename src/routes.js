4fcf9c38d0070f126b3fd8f073bc30f8c270531e
Lucas Lima <lukkasoliveiralima@gmail.com>
2022-08-03 18:34:47 -0300

    create not found page

--- src/routes.js
+++ src/routes.js
@@ -2,6 +2,7 @@ import Footer from 'components/Footer';
 import Nav from 'components/Nav';
 import Home from 'pages/Home';
 import Menu from 'pages/Menu';
+import NotFound from 'pages/NotFound';
 import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
 export default function AppRouter() {
@@ -11,6 +12,7 @@ export default function AppRouter() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/menu" element={<Menu />} />
+        <Route path='*' element={<NotFound />}/>
       </Routes>
       <Footer />
     </BrowserRouter>
